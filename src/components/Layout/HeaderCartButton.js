4501541252d59fecb3678bb4cf3327c6f7325001
fengbanjiazhu <562475805@qq.com>
2023-02-12 20:52:01 +1100

    add animation on cart with useEffect&useState

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,33 @@
-import React, { useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import CartIcon from "./CartIcon";
 import classes from "./HeaderCartButton.module.css";
 import cartData from "../../store/cartData";
 
 function HeaderCartButton(props) {
+  const [buttonBump, setButtonBump] = useState(false);
   const cart = useContext(cartData);
+  const { items } = cart;
 
-  const numberOfCartItems = cart.items.reduce((current, item) => {
+  const numberOfCartItems = items.reduce((current, item) => {
     return current + item.amount;
   }, 0);
+
+  const btnClasses = `${classes.button} ${buttonBump && classes.bump}`;
+
+  useEffect(() => {
+    if (items.length === 0) return;
+    setButtonBump(true);
+
+    const timer = setTimeout(() => {
+      setButtonBump(false);
+    }, 300);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
